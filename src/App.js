683a0c01da38bae97c8096e66b80c2afa0fb42ae
James <42342586+thehicksivist@users.noreply.github.com>
2021-01-14 11:57:00 +1000

    got scss working mostly

--- src/App.js
+++ src/App.js
@@ -1,6 +1,39 @@
 import './App.css';
+import './Styles/application.scss';
 
 function App() {
+    window.onload = function () {
+        console.log('doc loaded');
+
+        let profile = document.getElementById('pcards');
+        let past = document.getElementById('past');
+        let current = document.getElementById('current');
+        let tools = document.getElementById('tools');
+
+        profile.addEventListener('click', spin);
+        past.addEventListener('click', drop);
+        current.addEventListener('click', drop);
+        tools.addEventListener('click', drop);
+
+        let spun = false;
+
+        function spin() {
+            if (spun) {
+                this.style.transform = 'rotateY(0deg)';
+                spun = false;
+            } else {
+                this.style.transform = 'rotateY(180deg)';
+                spun = true;
+            }
+        }
+
+        function drop() {
+            this.classList.toggle('dropdown');
+            setTimeout(() => {
+                this.classList.toggle('dropdown');
+            }, 6000);
+        }
+    };
     return (
         <div className='App'>
             <header>
